7330c075a3ffa4d75787848718df227312bb8843
Manu <cv03029@ti.ues.edu.sv>
2022-04-27 19:21:48 -0600

    Initial commit

---
+++ app/src/main/java/ues/edu/sv/ctrlmed/MainActivity.java
@@ -0,0 +1,14 @@
+package ues.edu.sv.ctrlmed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
